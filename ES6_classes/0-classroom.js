6036b6979d0c47ee7aff9b9f1e9fa3236473daf5
MarioMelengu <10030@holbertonstudents.com>
2025-03-27 13:00:06 +0000

    added files

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,5 +1,9 @@
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-      this._maxStudentsSize = maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
   }
+  
+  get maxStudentsSize() {
+    return this._maxStudentsSize;
+  }
+}
