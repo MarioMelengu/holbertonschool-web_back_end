bc23f8f06d21f4bff9361beaa542f1bd494eec43
MarioMelengu <10030@holbertonstudents.com>
2025-03-27 11:22:49 +0000

    added files

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,11 @@
 /*eslint-disable*/
 export default function getBudgetObject(income, gdp, capita) {
-	const budget = {
-	  income,
-	  gdp,
-	  capita,
-	};
-
-	return budget;
-}
+    const budget = {
+      income: income,
+      gdp: gdp,
+      capita: capita,
+    };
+    
+    return budget;
+  }
+  
