e03920f094fa4e0b6d495c8ae31b48107c59abf9
MarioMelengu <10030@holbertonstudents.com>
2025-04-01 11:34:57 +0000

    added file

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,9 @@
-export default function getStudentsByLocation(students, city) {
-    return students.filter((student) => student.location === city);
-  }
+/*eslint-disable*/
+function getStudentsByLocation(students, city) {
+
+    if (!Array.isArray(students) || typeof city !== 'string') {
+        return [];
+    }
+
+    return students.filter(student => student.city === city);
+}
