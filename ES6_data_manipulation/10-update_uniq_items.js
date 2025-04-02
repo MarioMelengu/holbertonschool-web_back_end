ca65480307f96c2e2a33c9568eb187041580d706
MarioMelengu <10030@holbertonstudents.com>
2025-04-02 09:08:40 +0000

    added file

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,15 +1,11 @@
 /*eslint-disable*/
-export default function updateUniqueItems() {
+export default function updateUniqueItems(map) {
     if (!(map instanceof Map)) {
-          throw new Error("Cannot process");
-        }
-
-        for (let [key, value] of map) {
-
-          if (value === 1) {
-            map.set(key, 100);
-          }
-        }
-      p
-        return map;
+      throw new Error('Cannot process');
+    }
+    map.forEach((value, key) => {
+      if (value === 1) {
+        map.set(key, 100);
       }
+    });
+  }
