e9d6de888202342d57eb0f283dae16d3266f5397
MarioMelengu <10030@holbertonstudents.com>
2025-04-04 09:43:24 +0000

    added files

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 export default function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
-      const success = true;
-      if (success) {
+      const isApiSuccess = true;
+      if (isApiSuccess) {
         resolve({status: 200, body: 'Success'});
       } else {
         reject(new Error('The fake API is not working currently'));
